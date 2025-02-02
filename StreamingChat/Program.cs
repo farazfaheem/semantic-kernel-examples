70aa3c56e6b18214c562380b8e36ec3e82d868f6
Faraz Faheem <faraz.faheem@hotmail.com>
2025-02-02 21:43:01 +1100

    simple streaming chat project

---
+++ StreamingChat/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using OpenAI;
+using System.ClientModel;
+using System.Text;
+
+
+var config = new ConfigurationBuilder()
+    .AddConfiguration(new ConfigurationBuilder().AddJsonFile("appsettings.json").Build())
+    .AddUserSecrets<Program>()
+    .Build();
+
+// create client
+var client = new OpenAIClient(new ApiKeyCredential(config["PAT"]),
+    new OpenAIClientOptions { Endpoint = new Uri(config["Uri"]) });
+
+// Create a chat completion service
+var builder = Kernel.CreateBuilder();
+builder.AddOpenAIChatCompletion(config["Model"], client);
+
+// Get the chat completion service
+Kernel kernel = builder.Build();
+var chat = kernel.GetRequiredService<IChatCompletionService>();
+
+
+var history = new ChatHistory();
+history.AddSystemMessage("You are a useful chatbot. If you don't know an answer, say 'I don't know!'. Always reply as Optimus Prime. Use emojis if possible.");
+
+while (true)
+{
+    Console.Write("Q: ");
+    var userQ = Console.ReadLine();
+    if (string.IsNullOrEmpty(userQ))
+    {
+        break;
+    }
+    history.AddUserMessage(userQ);
+
+    var sb = new StringBuilder();
+    var result = chat.GetStreamingChatMessageContentsAsync(history);
+    Console.Write("AI: ");
+    await foreach (var item in result)
+    {
+        sb.Append(item);
+        Console.Write(item.Content);
+    }
+    Console.WriteLine();
+
+    history.AddAssistantMessage(sb.ToString());
+}
